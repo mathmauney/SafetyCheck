7446ae492880d876f5010a0c7731c808a79d0338
Alex Mauney <mathmauney@gmail.com>
2020-04-29 15:52:41 -0400

    another try at ssl

--- app.py
+++ app.py
@@ -342,4 +342,4 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
-    app.run(port=3000, ssl_context=ssl_context)
+    app.run(port=3000, ssl_context='ad_hoc')
