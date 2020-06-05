795097bf462b2fbad9bc154a37d36efd1d5db0e7
mathmauney <mathmauney@gmail.com>
2020-06-05 14:28:59 -0400

    Test changes for OAuth

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ main_db = client.test
 # main_db = my_client['safety']
 users = main_db['users']
 settings = main_db['settings']    # No settings yet, keeping this in case I need it.
-
+tokens = main_db['tokens']
 
 def add_user(user_id):
     """Add a new user to the database."""
