1edeeb1a152c6bf69fa22a1dfc9f06ed8a577e9c
mathmauney <mathmauney@gmail.com>
2020-06-06 09:38:40 -0400

    Prevent overaddition of db entries

--- database.py
+++ database.py
@@ -10,12 +10,26 @@ users = main_db['users']
 settings = main_db['settings']    # No settings yet, keeping this in case I need it.
 tokens = main_db['tokens']
 
+
 def add_user(user_id):
     """Add a new user to the database."""
     init_dict = {"slack_id": user_id}
     users.insert_one(init_dict)
 
+
 def add_token(teamID, token):
-    init_dict = {"teamID": teamID,
-                 "token": token}
-    tokens.insert_one(init_dict)
+    check = {"teamID": teamID}
+    entry = tokens.find_one(check)
+    if entry is None:
+        init_dict = {"teamID": teamID,
+                     "token": token}
+        tokens.insert_one(init_dict)
+    else if entry['token'] != token:
+        update_dict = {'$set': {'token': token}
+        token.update_one(entry, update_dict)
+
+
+def get_token(teamID):
+    find_dict = {'teamID': teamID}
+    entry = tokens.find_one(find_dict)
+    return entry['token']
